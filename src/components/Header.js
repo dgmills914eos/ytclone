d5e06de4377753a0abf014e213b0fa428af5d9a9
Dan Mills <dgmills914eos@gmail.com>
2020-05-06 19:13:56 -0500

    finish header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {AntDesign} from '@expo/vector-icons';
-import Constant from 'expo-constants';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AntDesign, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Constant from "expo-constants";
 
 export default function Header() {
+  const mycolor = "#212121";
   return (
-    <View style={{
-        marginTop:Constant.statusBarHeight,
-        height:40,
-        backgroundColor:"skyblue"
-    }}>
-      <Viewstyle={{
-          flexDirection:"row"
-      }}>
-          <AntDesign name="youtube", size={28} color="red" />
-          <Text style={{
-              fontSize:22
-          }}>YouTube</Text>
+    <View
+      style={{
+        marginTop: Constant.statusBarHeight,
+        height: 45,
+        backgroundColor: "white",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        elevation: 4,
+        shadowOffset: { width: 10, height: 10 },
+        shadowColor: "black",
+        shadowOpacity: 1.0,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          margin: 5,
+        }}
+      >
+        <AntDesign
+          style={{ marginLeft: 10 }}
+          name="youtube"
+          size={28}
+          color="red"
+        />
+        <Text
+          style={{
+            color: mycolor,
+            fontSize: 22,
+            marginLeft: 5,
+            fontWeight: "bold",
+          }}
+        >
+          YouTube
+        </Text>
       </View>
-      <View></View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          width: 140,
+          margin: 5,
+        }}
+      >
+        <Ionicons name="md-videocam" size={32} color={mycolor} />
+        <Ionicons name="md-search" size={32} color={mycolor} />
+        <MaterialIcons name="account-circle" size={32} color={mycolor} />
+      </View>
     </View>
   );
 }
